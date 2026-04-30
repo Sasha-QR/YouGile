adfc418b9c51a347b09df68166e51690b0a71965
Sasha-QR <sashynya1995@yandex.ru>
2026-04-30 19:23:15 +0200

    versia3

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,5 +1,7 @@
 import pytest
 import allure
+import uuid
+
 from pages.auth_page import AuthPage
 from pages.main_page import MainPage
 
@@ -8,6 +10,10 @@ EMAIL = "твой логин"
 PASSWORD = "твой пароль"
 
 
+
+@allure.epic("UI")
+@allure.feature("Авторизация")
+@allure.story("Успешный вход")
 @allure.title("Успешная авторизация")
 @pytest.mark.ui
 def test_login_success(driver):
@@ -19,6 +25,10 @@ def test_login_success(driver):
     assert auth.is_logged_in()
 
 
+
+@allure.epic("UI")
+@allure.feature("Главная страница")
+@allure.story("Загрузка")
 @allure.title("Загрузка главной страницы после логина")
 @pytest.mark.ui
 def test_main_page_loaded(driver):
@@ -31,13 +41,32 @@ def test_main_page_loaded(driver):
     assert main.is_loaded()
 
 
-@allure.title("Проверка успешной авторизации (UI)")
+
+@allure.epic("UI")
+@allure.feature("Навигация")
+@allure.story("Переход после логина")
+@allure.title("Проверка URL после авторизации")
 @pytest.mark.ui
-def test_user_logged_in(driver):
+def test_url_after_login(driver):
     auth = AuthPage(driver)
-    main = MainPage(driver)
 
     auth.open()
     auth.login(EMAIL, PASSWORD)
 
-    assert main.is_loaded()
+    assert "login" not in driver.current_url.lower()
+    assert "yougile" in driver.current_url.lower()
+
+
+
+@allure.epic("UI")
+@allure.feature("Навигация")
+@allure.story("Редирект после логина")
+@allure.title("Проверка редиректа после логина")
+@pytest.mark.ui
+def test_redirect_after_login(driver):
+    auth = AuthPage(driver)
+
+    auth.open()
+    auth.login(EMAIL, PASSWORD)
+
+    assert "login" not in driver.current_url.lower()
